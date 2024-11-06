a6830dd0d622c56c8465a9ba09e896498b404d24
Goggles <82652626+gogglesgogs@users.noreply.github.com>
2024-11-06 15:55:06 +0700

    Update [[img]].js

--- functions/api/img/[[img]].js
+++ functions/api/img/[[img]].js
@@ -1,5 +1,5 @@
 const formats = ["png", "jpg", "webp", "avif"];
-const sizes = ["32", "64", "128", "196", "256", "300", "400", "500"];
+const sizes = [32, 64, 128, 196, 256, 300, 400, 500];
 
 function response(res, code) {
   return new Response(JSON.stringify(res, { status: code }))
@@ -8,11 +8,12 @@ function response(res, code) {
 export function onRequest(context) {
   const format = context.params.img[0];
   const size = context.params.img[1];
-  
+  if (!format || !size)
+    return response(`Please provide format and size. Available formats: ${formats}. Available sizes: ${sizes}.`)
   if (!formats.includes(format)) 
-    return response(`Invalid format: ${format}`, 400);
+    return response(`Invalid format: ${format}.`, 400);
   if (!formats.includes(size))
-    return response(`Invalid size: ${size}`, 400);
+    return response(`Invalid size: ${size}.`, 400);
   
-  return response(`goggles-${format}-${size}.${format}`, 200);
+  return response(`Returned image: goggles-${format}-${size}.${format}`, 200);
 }
