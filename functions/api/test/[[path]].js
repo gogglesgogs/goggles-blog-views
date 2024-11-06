cea689eb1bcd8db033373e8f75edf74f9587964c
Goggles <82652626+gogglesgogs@users.noreply.github.com>
2024-11-06 16:48:35 +0700

    Update [[path]].js

--- functions/api/test/[[path]].js
+++ functions/api/test/[[path]].js
@@ -1,9 +1,13 @@
 export function onRequest(context) {
   return new Response(JSON.stringify({ 
-    data: JSON.stringify(context),
-    params: JSON.stringify(context.params),
+    return_data: JSON.stringify(context),
+    
+    params_data: context.params,
+    params_json: JSON.stringify(context.params),
     params_type: typeof context.params,
-    path: JSON.stringify(context.params.path),
+    
+    path_data: context.params.path,
+    path_json: JSON.stringify(context.params.path),
     path_type: typeof context.params.path,
   }));
 }
