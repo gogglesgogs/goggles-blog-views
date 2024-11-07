91df83cba772f27e6c1eee534e3afc5099ad9d46
Goggles <82652626+gogglesgogs@users.noreply.github.com>
2024-11-07 15:25:13 +0700

    Update [[img]].js

--- functions/api/[[img]].js
+++ functions/api/[[img]].js
@@ -5,8 +5,8 @@ const valid_params = {
   sizes: sizes,
 };
 
-function response(res, code) {
-  return new Response(JSON.stringify(res, null, 4), { status: code });
+function response(res) {
+  return new Response(JSON.stringify(res, null, 2));
 };
   
 export function onRequest(context) {
@@ -14,7 +14,7 @@ export function onRequest(context) {
     return response({
       error: 'Please provide /format and /size',
       valid_params,
-    }, 400);
+    });
   
   const format = context.params.img[0];
   const size = parseInt(context.params.img[1]);
@@ -23,30 +23,30 @@ export function onRequest(context) {
     return response({ 
       error: 'Please provide /format',
       valid_params,
-    }, 400);
+    });
 
   if (!size)
     return response({
       error: 'Please provide /size',
       valid_params,
-    }, 400);
+    });
   
   if (!formats.includes(format)) 
     return response({
       error: `Invalid format: ${format}`,
       valid_params,
-    }, 400);
+    });
   
   if (!sizes.includes(size))
     return response({
       error: `Invalid size: ${size}`,
       valid_params,
-    }, 400);
+    });
   
   return response({
     url: `http://goggles.pages.dev/image/goggles-${format}-${size}.${format}`,
     format: format,
     size: size,
     valid_params,
-  }, 200);
+  });
 }
