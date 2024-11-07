7e1b2e3a513adef0b76742cf542f9edda2b6bf7f
Goggles <82652626+gogglesgogs@users.noreply.github.com>
2024-11-07 11:59:46 +0700

    Update index.js

--- functions/api/index.js
+++ functions/api/index.js
@@ -2,6 +2,6 @@ export function onRequest(context) {
   return new Response(JSON.stringify({
     data: JSON.stringify(context),
     env: `Hello ${context.env.hello}`,
-    kv: `Hello ${context.env.kv.get('hello')}`,
+    kv: `Hello ${await context.env.kv.get('hello')}`,
   }));
 }
