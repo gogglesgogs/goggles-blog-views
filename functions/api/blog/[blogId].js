f9772a33f56ddb169b51937198865bd5da5233d7
Goggles <82652626+gogglesgogs@users.noreply.github.com>
2024-11-14 12:15:18 +0700

    Update [blogId].js

--- functions/api/blog/[blogId].js
+++ functions/api/blog/[blogId].js
@@ -1,4 +1,6 @@
 export async function onRequestGet(context) {
   const view = await context.env.BLOG_VIEWS.get("blog1");
-  return new Response(JSON.stringify({ view }));
+  return new Response(JSON.stringify({ 
+    id: context.params.blogId,
+    view }));
 }
