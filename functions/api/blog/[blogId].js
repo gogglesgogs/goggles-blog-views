8081295e2cfa308a1254751c5be69c3042742a16
Goggles <82652626+gogglesgogs@users.noreply.github.com>
2024-11-14 16:07:37 +0700

    Update [blogId].js

--- functions/api/blog/[blogId].js
+++ functions/api/blog/[blogId].js
@@ -1,7 +1,13 @@
 const blogs = ["blog1", "blog2"];
 
 export async function onRequestGet(context) {
-  const view = await context.env.BLOG_VIEWS.get("blog1");
+  const view = await context.env.BLOG_VIEWS.get(context.params.blogId);
+  if (!view) {
+    return new Response(JSON.stringify({
+      error: 404,
+      message: `${context.params.blogId}: blog not found`
+    }));
+  }
   return new Response(JSON.stringify({ 
     id: context.params.blogId,
     views: view,
