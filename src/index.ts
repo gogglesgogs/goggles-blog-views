d389eae95ff798f68f0bcf78c2fe03db6abd8b35
gogglesgogs <ngkhoa0075@gmail.com>
2024-11-15 20:10:11 +0700

    allow all origins

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,7 @@ function response(res: res, opts?: ResponseInit): Response {
 	return new Response(JSON.stringify(res), {
 		headers: {
 			'Content-Type': 'application/json',
-			//! SET TO PORTFOLIO DOMAIN
-			'Access-Control-Allow-Origin': 'https://goggles.pages.dev',
+			'Access-Control-Allow-Origin': '*',
 			'Access-Control-Allow-Methods': 'GET, POST',
 			'Access-Control-Allow-Headers': 'Content-Type',
 		},
